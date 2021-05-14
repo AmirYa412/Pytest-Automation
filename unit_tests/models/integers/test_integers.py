ae207bd437011cd3f527beeb66052a1394b777b9
AmirYa412 <AmirYA>
2021-05-15 01:08:49 +0300

    minor fix

--- unit_tests/models/integers/test_integers.py
+++ unit_tests/models/integers/test_integers.py
@@ -9,17 +9,17 @@ class TestNumbers:
         x = 1
         y = 1
         expected_value = x + y
-        assert expected_value == Integers().add(x=x, y=y)
+        assert expected_value == Integers.add(x=x, y=y)
 
     def test_divide_numbers(self):
         x = 10
         y = 2
         expected_value = x / y
-        assert expected_value == Integers().divide(x=x, y=y)
+        assert expected_value == Integers.divide(x=x, y=y)
 
     def test_zero_division_error(self):
         x = 10
         y = 0
         with pytest.raises(ZeroDivisionError):
-            Integers().divide(x=x, y=y)
+            Integers.divide(x=x, y=y)
 
