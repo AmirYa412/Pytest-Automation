df453976d5bcd450ea5bb890ef85f0be48eaee85
AmirYa412 <AmirYA>
2021-05-16 15:26:33 +0300

    google login func

--- unit_tests/models/google/google_photos.py
+++ unit_tests/models/google/google_photos.py
@@ -5,14 +5,18 @@ class GooglePhotos:
     def __init__(self, user, password):
         self.user = user
         self.password = password
+        self.session = requests.Session()
 
-    def get_user_google_photos(self):  # In our test we will mock this function's return value to avoid Google login
+    def google_login(self):
         data = {
             "username": self.user,
             "password": self.user
         }
-        session = requests.Session()
-        response = session.post("https://www.google.com/api/auth", data=data)
-        assert response.status_code == 200
-        response = requests.get("https://www.google.com/api/google/photos")
+        response = self.session.post("https://www.google.com/api/auth", data=data)
+        response.raise_for_status()
+
+    def get_user_google_photos(self):  # In our test we will mock this function's return value to avoid google_login()
+        self.google_login()
+        response = self.session.get("https://www.google.com/api/google/photos")
+        response.raise_for_status()
         return response.json()
